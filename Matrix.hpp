751a904a2c3bdabb7a1841d49557639c3f225b3c
Amirali Danai <danaiamirali@gmail.com>
2023-06-19 23:44:12 -0400

    Update Matrix.hpp

--- Matrix.hpp
+++ Matrix.hpp
@@ -26,14 +26,10 @@ class Matrix {
     m and n must be positive integers
     m and n must be less than MAX_DIM
     */
+    friend class SquareMatrix;
 
     int m, n;
     int **matrix = nullptr;
-
-        // Converts 1D Index to 2D Index
-        std::pair<int, int> index(int i) const {
-            return std::pair<int, int> (i / n, i % n);
-        }
 
     public:
         // Constructor with specified dimensions
@@ -55,11 +51,18 @@ class Matrix {
         // Default constructor
         Matrix() : m(0), n(0) {}
 
+        // Copy constructor
+        // Deep Copy
+        Matrix(const Matrix& matrix) {
+            copyAll(matrix);
+        }
+
         ~Matrix() {
-            for (int i = 0; i < m; i++) {
-                delete[] matrix[i];
-            }
-            delete[] matrix;
+            deleteMatrix();
+        }
+
+        std::pair<int, int> getDimensions() const {
+            return std::pair<int, int> (m, n);
         }
 
         // Fills matrix with random integers from 0 to 9
@@ -89,16 +92,16 @@ class Matrix {
                 }
 
             return this;
-    }
+            }
 
-    int numbersCount = numbers.size();
-    for (int i = 0; i < m * n && i < numbersCount; i++) {
-        std::pair<int, int> ind = index(i);
-        matrix[ind.first][ind.second] = numbers[i];
-    }
+            int numbersCount = numbers.size();
+            for (int i = 0; i < m * n && i < numbersCount; i++) {
+                std::pair<int, int> ind = index(i);
+                matrix[ind.first][ind.second] = numbers[i];
+            }
 
-    return this;
-        }
+            return this;
+    }
 
         int* operator[](int i) const {
             return matrix[i];
@@ -116,13 +119,186 @@ class Matrix {
             return os;
         }
 
+        // Overloaded assignment operator
+        // Deep Copy
+        Matrix *operator=(const Matrix& matrix) {
+            if (this == &matrix) {
+                return this;
+            }
+
+            deleteMatrix();
+
+            copyAll(matrix);
+        }
+
+        // Returns reduced echelon form of matrix
+        // Does not modify original matrix
+        Matrix ref() {
+            Matrix ref = *this;
+
+            // Recursively reduce function to echelon form
+            return *reduce(&ref);
+        }
+
+        Matrix *reduce(Matrix *ref) {
+            // Base Case: Matrix has 1 row
+            if (ref->m == 1) {
+                return ref;
+            }
+            // Else: Matrix has more than 1 row
+            else {
+                if (ref->isZeroCol(0)) {
+                    return reduce(ref->cropFirstCol());
+                }
+                else {
+                    // Find first nonzero
+                    int *firstCol = ref->getCol(0);
+                    for (int i = 0; i < m; i++) {
+                        if (firstCol[i] != 0) {
+                            // If first nonzero is not in first row, swap
+                            if (i != 0) {
+                                ref->swapRows(0, i);
+                            }
+                            break;
+                        }
+                    }
+                    // Create zeros in all other rows in first column
+                    for (int i = 1; i < m; i++) {
+                        // R = R + (-a/b)R1
+                        int *row = ref->getRow(i);
+                        if (row[0] == 0) {
+                            continue;
+                        }
+                        /*
+                        TO-DO
+                        */
+                    }
+
+                    reduce(ref->cropFirstCol());
+                }
+
+                return ref;
+            }
+            // If first column is all 0s
+                // Skip col and reduce submatrix, then return
+            // If first column has nonzero
+                // Pick first nonzero, if isn't first row, swap with 1st
+                // Create zeros in all other rows in first column
+            // Reduce submatrix
+            // Return
+        }
+
+    private:
+        /*
+         * Helper class functions
+        */   
+
+        // Converts 1D Index to 2D Index
+        std::pair<int, int> index(int i) const {
+            return std::pair<int, int> (i / n, i % n);
+        }
+
+        int* getCol(int col) const {
+            int *column;
+            for (int i = 0; i < m; i++) {
+                column[i] = matrix[i][col];
+            }
+
+            return column;
+        }
+
+        int* getRow(int row) const {
+            return matrix[row];
+        }
+
+        bool isZeroCol(int col) const {
+            int *c = getCol(col);
+            for (int i = 0; i < n; i++) {
+                if (c[i] != 0) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        Matrix *swapRows(int row1, int row2) {
+            int *temp = matrix[row1];
+            matrix[row1] = matrix[row2];
+            matrix[row2] = temp;
+
+            return this;
+        }
+
+        // Returns new matrix with column x removed
+        Matrix popCol(int x) {
+            Matrix newMatrix = Matrix(m, n - 1);
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (j == x) {
+                        continue;
+                    }
+                    else if (j < x) {
+                        newMatrix[i][j] = matrix[i][j];
+                    }
+                    else {
+                        newMatrix[i][j - 1] = matrix[i][j];
+                    }
+                }
+            }
+
+            return newMatrix;
+        }
+
+        // Returns pointer to same matrix with first column cropped
+        Matrix *cropFirstCol() {
+            for (int i = 0; i < m; i++) {
+                matrix[i]++;
+            }
+            n--;
+        }
+
+        Matrix *copyAll(const Matrix &matrix) {
+            this->m = matrix.m;
+            this->n = matrix.n;
+
+            this->matrix = new int*[m];
+            for (int i = 0; i < m; i++) {
+                this->matrix[i] = new int[n];
+            }
+
+            return this;
+        }        
+
+        void deleteMatrix() {
+                for (int i = 0; i < m; i++) {
+                    delete[] matrix[i];
+                }
+                delete[] matrix;
+            }
+
+
+};
+
 class SquareMatrix : public Matrix {
     public:
         SquareMatrix(int n) : Matrix(n, n) {}
 
+        SquareMatrix(Matrix m) {
+            std::pair<int, int> dim = m.getDimensions();
+            if (dim.first != dim.second) {
+                throw DIM_ERROR();
+            }
+
+            this->n = dim.first;
+
+            this->matrix = m.matrix;
+        }
+
         // Calculates determinant of matrix
+        // Intelligently chooses between cofactor expansion and row reduction
         int determinant() {
-            if ()
+            
         }
 };
 
